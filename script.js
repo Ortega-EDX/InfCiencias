9f4955e32cb286bc5b2b524208eb81520bd43a8a
Ortega EDX <jortega@mail.colsam.edu.co>
2026-05-05 10:17:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Esta línea es la primera prueba de que JavaScript está funcionando
+console.log("¡El script.js ha cargado correctamente!");
+// Ejemplo: Mostrar un saludo al cargar la página
+window.onload = function() {
+alert("¡Bienvenido/a a nuestra página web!");
+};
